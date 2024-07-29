4dcb404ff48b05f70e1a383c35ba7ea9059cf600
getuliogutemberg <getulio.dev@gmail.com>
2024-07-29 16:32:02 -0300

    adsa

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
   useEffect(() => {
     // Conectar ao servidor WebSocket
-    const ws = new WebSocket('ws://esp32-data-api-1.onrender.com/');
+    const ws = new WebSocket('ws:esp32-data-api-1.onrender.com');
     // Evento de conexão estabelecida
     ws.addEventListener('open', function(event) {
       console.log('Conectado ao WebSocket');
